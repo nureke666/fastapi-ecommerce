63489dbd0b5cb2b379d1c270e96152008f260d24
Nurdaulet <baglanovbaglan227@gmail.com>
2026-02-08 21:53:29 +0500

    feat: add product and category routes, DB schema, and basic main setup

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.routers import category, products
+
+app = FastAPI()
+
+@app.get('/')
+async def welcome() -> dict:
+    return {'message': 'My ecommerce API'}
+
+app.include_router(category.router)
+app.include_router(products.router)
---
+++ app/routers/category.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix='/category', tags=['category'])
+
+@router.get('/all_categories')
+async def get_all_categories():
+    pass
+
+
+@router.post('/create')
+async def create_category():
+    pass
+
+
+@router.put('/update')
+async def update_category():
+    pass
+
+
+@router.delete('/delete')
+async def delete_category():
+    pass
